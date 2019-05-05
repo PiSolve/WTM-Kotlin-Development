840fd07fbf533309fcf4a6c393f1186b0257dab6
renu_yadav <renuyadav20@gmail.com>
2019-05-05 16:11:56 +0800

    adding a title to the movie

--- app/src/main/java/com/wtm/kotlin/development/MainActivity.kt
+++ app/src/main/java/com/wtm/kotlin/development/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        supportActionBar?.title = "Kotlin Development"
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
