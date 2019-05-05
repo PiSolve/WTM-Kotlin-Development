e0b8ebe23613e0e2db7291b265654ff598a65fc4
renu_yadav <renuyadav20@gmail.com>
2019-05-05 10:03:17 +0800

    adding all three clicks for buttons

--- app/src/main/java/com/wtm/kotlin/development/MainActivity.kt
+++ app/src/main/java/com/wtm/kotlin/development/MainActivity.kt
@@ -5,8 +5,14 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DateFormat
+import java.util.*
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
@@ -26,11 +32,34 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    fun showToast(view: View) {
+        val toast = Toast.makeText(this, "Showing a Toast", Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countForMe(view: View) {
+        val mainTextView = findViewById<TextView>(R.id.textView)
+        val countInString = mainTextView.text.toString()
+        var countInNo = Integer.parseInt(countInString)
+        countInNo += 1
+        mainTextView.text = countInNo.toString()
+
+
+    }
+
+    fun showDate(view: View) {
+        val date = Calendar.getInstance().time
+        val formatteddate = DateFormat.getInstance().format(date)
+        val mainTextView = findViewById<TextView>(R.id.textView)
+        mainTextView.text = formatteddate.toString()
+
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
